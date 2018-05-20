1c5480eee0fa577f7a1ecce28a1e4ea718f5f622
octoshrimpy <octavo.acapella@gmail.com>
2018-05-20 00:15:40 -0600

    better data management, working handlebars rendering

--- js/config.js
+++ js/config.js
@@ -1,39 +1,138 @@
-//setup data for the resume
-var data = {
+/*
+██████   █████  ████████  █████
+██   ██ ██   ██    ██    ██   ██
+██   ██ ███████    ██    ███████
+██   ██ ██   ██    ██    ██   ██
+██████  ██   ██    ██    ██   ██
+*/
 
-  heading: {
-    name: "Marcos Jones",
-    subtitle: "Frontend developer",
-    email: 'email.mail.com'
+var data = new Object();
+
+data.heading = {
+  name: "Marchos Jones",
+  subtitle: "Frontend developer",
+  email: 'hitlerdidnothingwrong@hotmail.com'
+};
+
+data.objective = {
+  description: ""
+};
+
+data.education = {
+  school: "",
+  city: "",
+  degree: "",
+  gradDate: "",
+  gpa: "",
+  honors: "",
+  deansList: "" // number of semesters
+};
+
+data.projects = {
+  projInfo: {
+    projName: "",
+    position: "",
+    startEnd: "",
+    location: ""
   },
+  descriptions: {
+    description: ""
+  }
+};
 
-  objective: {
+data.leadership = {
+  organization: {
+    organizationInfo: {
+      organizationName: "",
+      position: "",
+      startEnd: "",
+      location: ""
+    },
+    descriptions: {
+      description: ""
+    }
+  }
+};
 
-  },
+data.work = {
+  job: {
+    jobInfo: {
+      title: "",
+      company: "",
+      startEnd: "",
+      location: ""
+    },
+    descriptions: {
+      description: ""
+    }
+  }
+};
 
-  jobs: [
-    {
-      jobInfo:{
-        title:"",
-        company:"",
-        times:"",
-        location:""
-      },
-      descriptions:{
-        desc:"",
-        desc:"",
-        desc:""
+data.skills = {
+  skills: {
+    skill: {
+      skillLevel: "",
+      descriptions: {
+        description: ""
       }
     }
-  ]
+  }
 };
+
+data.awards = {
+  awards: {
+    award: {
+      awardName: "",
+      time: "",
+      descriptions: {
+        description: ""
+      }
+    }
+  }
+};
+
+data.references = {
+  contact: {
+    name: "",
+    position: "",
+    company: "",
+    phone: "",
+    email: "",
+    address: ""
+  }
+};
+
+/*
+ ██████  ██████  ███    ██ ███████ ██  ██████
+██      ██    ██ ████   ██ ██      ██ ██
+██      ██    ██ ██ ██  ██ █████   ██ ██   ███
+██      ██    ██ ██  ██ ██ ██      ██ ██    ██
+ ██████  ██████  ██   ████ ██      ██  ██████
+*/
 
 //TODO: add config targeting each section,
 //: so as to hide/show individual ones
 
+var config = {
+  // chronological: 'chrono'
+  // functional   : 'funct'
+  organizeBy: "chrono",
+
+  //write as comma-separated strings:
+  //heading, objective, education, projects,
+  //leadership, work, skills, awards, references
+  hideSections: {}
+}
 
 //begin processing everything together.
 
+/*
+██       ██████   ██████  ██  ██████
+██      ██    ██ ██       ██ ██
+██      ██    ██ ██   ███ ██ ██
+██      ██    ██ ██    ██ ██ ██
+███████  ██████   ██████  ██  ██████
+*/
 
 $(document).ready(function() {
   //Retrieve the template data from the HTML .
