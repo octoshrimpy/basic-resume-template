e4c182c25e67ef607830590a4ee5324a3d898c36
octoshrimpy <octavo.acapella@gmail.com>
2018-05-19 22:01:01 -0600

    changing method of handling variables.

--- js/config.js
+++ js/config.js
@@ -1,3 +1,40 @@
+var data = {
+
+  heading: {
+    name: "Marcos Jones",
+    subtitle: "Frontend developer",
+    email: 'email.mail.com'
+  }
+
+  objective: {
+
+  }
+
+  jobs: {[
+    {
+      jobInfo:{
+        title:"",
+        company:"",
+        times:"",
+        location:""
+      },
+      descriptions:{
+        desc:"",
+        desc:"",
+        desc:""
+      }
+    },
+    {
+
+    },
+    {
+
+    }
+  ]}
+};
+
+
+
 $(document).ready(function() {
   //Retrieve the template data from the HTML .
   var source = $('#resume-template').html();
@@ -8,6 +45,10 @@ $(document).ready(function() {
   //set variables
   //vars hiding in ./main.js
   //behind a variable called data
+  $.getJSON('.data.json')
+    .done(function(data){
+      var data = data;
+    });
 
   //attach variables to compiled source
   var output = template(data);
