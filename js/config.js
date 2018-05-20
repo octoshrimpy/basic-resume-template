1618c3a0a4eb4994024207893d7d8d658d53c2c6
Reed Thurber <rthurber3@gmail.com>
2018-05-20 01:39:44 -0600

    Updated HTML and JS Data File

--- js/config.js
+++ js/config.js
@@ -1,13 +1,152 @@
+/*
+██████   █████  ████████  █████
+██   ██ ██   ██    ██    ██   ██
+██   ██ ███████    ██    ███████
+██   ██ ██   ██    ██    ██   ██
+██████  ██   ██    ██    ██   ██
+*/
+
+var data = new Object();
+
+data.heading = {
+  loop: false,
+  name: "Marchos Jones",
+  subtitle: "Frontend developer",
+  email: 'hitlerdidnofingwrng@grandmamail.com'
+};
+
+data.objective = {
+  loop: false,
+  descriptions:  {
+    description: ""
+  }
+};
+
+data.education = {
+  loop: false,
+  school: "",
+  city: "",
+  degree: "",
+  gradDate: "",
+  gpa: "",
+  honors: "",
+  deansList:"" // number of semesters
+};
+
+data.projects = {
+  loop: true,
+  projInfo: {
+    projName: "",
+    position: "",
+    startEnd: "",
+    location: ""
+  },
+  descriptions: {
+    description: ""
+  }
+};
+
+data.leadership = {
+  loop: true,
+  organization: {
+    organizationInfo: {
+      organizationName: "",
+      position: "",
+      startEnd: "",
+      location: ""
+    },
+    descriptions: {
+      description: ""
+    }
+  }
+};
+
+data.work = {
+  loop: true,
+  job: {
+    jobInfo: {
+      title: "",
+      company: "",
+      startEnd: "",
+      location: ""
+    },
+    descriptions: {
+      description: ""
+    }
+  }
+};
+
+data.skills = {
+  loop: true,
+  skill: {
+    skillLevel: "",
+    descriptions: {
+      description: ""
+    }
+  }
+};
+
+data.awards = {
+  loop: true,
+  award: {
+    awardName: "",
+      time: "",
+      descriptions: {
+        description: ""
+    }
+  }
+};
+
+data.references = {
+  loop: true,
+  contact: {
+    name: "",
+    position: "",
+    company: "",
+    phone: "",
+    email: "",
+    address: ""
+  }
+};
+
+/*
+ ██████  ██████  ███    ██ ███████ ██  ██████
+██      ██    ██ ████   ██ ██      ██ ██
+██      ██    ██ ██ ██  ██ █████   ██ ██   ███
+██      ██    ██ ██  ██ ██ ██      ██ ██    ██
+ ██████  ██████  ██   ████ ██      ██  ██████
+*/
+
+//TODO: add config targeting each section,
+//: so as to hide/show individual ones
+
+var config = {
+  // chronological: 'chrono'
+  // functional   : 'funct'
+  organizeBy: "chrono",
+
+  //write as comma-separated strings:
+  //heading, objective, education, projects,
+  //leadership, work, skills, awards, references
+  hideSections: {}
+}
+
+//begin processing everything together.
+
+/*
+██       ██████   ██████  ██  ██████
+██      ██    ██ ██       ██ ██
+██      ██    ██ ██   ███ ██ ██
+██      ██    ██ ██    ██ ██ ██
+███████  ██████   ██████  ██  ██████
+*/
+
 $(document).ready(function() {
   //Retrieve the template data from the HTML .
   var source = $('#resume-template').html();
 
   //compile the template engine
   var template = Handlebars.compile(source);
-
-  //set variables
-  //vars hiding in ./main.js
-  //behind a variable called data
 
   //attach variables to compiled source
   var output = template(data);
