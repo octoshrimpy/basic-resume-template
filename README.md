6761ac65128ae1524293dfa5c95a81401a551c42
octoshrimpy <octavo.acapella@gmail.com>
2022-04-05 18:52:30 -0600

    basics done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-basic-resume-template
+# Basic resume template (using handlebars)
+
+Drop all your data into the `data.json`, and then drop everything into your site.
--- js/config.js
+++ js/config.js
@@ -1,32 +1,10 @@
 //setup data for the resume
-var data = {
 
-  heading: {
-    name: "Marcos Jones",
-    subtitle: "Frontend developer",
-    email: 'email.mail.com'
-  },
 
-  objective: {
+$.getJSON("data.json", function(json) {
+  data = json
+});
 
-  },
-
-  jobs: [
-    {
-      jobInfo:{
-        title:"",
-        company:"",
-        times:"",
-        location:""
-      },
-      descriptions:{
-        desc:"",
-        desc:"",
-        desc:""
-      }
-    }
-  ]
-};
 
 //TODO: add config targeting each section,
 //: so as to hide/show individual ones
