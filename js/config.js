2bbca34b23d826886452dbefe14bb84120d7e22b
octoshrimpy <octavo.acapella@gmail.com>
2022-04-12 12:23:54 -0600

    adds changes

--- js/config.js
+++ js/config.js
@@ -1,29 +1,27 @@
-//setup data for the resume
 
-
-$.getJSON("data.json", function(json) {
-  data = json
-});
-
-
-//TODO: add config targeting each section,
 //: so as to hide/show individual ones
-
 
 //begin processing everything together.
 
-
 $(document).ready(function() {
-  //Retrieve the template data from the HTML .
-  var source = $('#resume-template').html();
 
-  //compile the template engine
-  var template = Handlebars.compile(source);
+  // fetch json
+  fetch("./data.json")
+    .then(response => response.json())
+    .then(data => {
+    
+      //Retrieve the template data from the HTML .
+      var source = $('#resume-template').html();
+    
+      //compile the template engine
+      var template = Handlebars.compile(source);
+    
+      //attach variables to compiled source
+      var output = template(data);
+    
+      //add to body
+      $(document.body).append(output);
 
-  //attach variables to compiled source
-  var output = template(data);
-
-  //add to body
-  $(document.body).append(output);
-
+    })
+  
 });
